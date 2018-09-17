2eff341ad806542479cb4e5cbdf79716faee389e
Craig Barry <s3601725@student.rmit.edu.au>
2018-09-17 20:19:21 +1000

    Selected features using chi2

--- task1.py
+++ task1.py
@@ -3,9 +3,12 @@ import numpy as np;
 import sklearn as sk;
 
 from sklearn import tree
+from sklearn import ensemble
 from sklearn import preprocessing
 from sklearn import metrics
 from sklearn import model_selection
+from sklearn import cluster
+from sklearn.feature_selection import SelectKBest, f_classif, chi2
 from sklearn.model_selection import train_test_split, cross_val_score;
 
 import graphviz
@@ -27,11 +30,29 @@ def encodeClassifiers(data, cols):
 #Cleans up NaN values in dataset
 def cleanDataset(data):
 	for col in list(data):
-		im = preprocessing.Imputer(strategy='median')
+		im = preprocessing.Imputer(strategy='mean')
 		im.fit(data[[col]]);
 		data[[col]] = im.transform(data[[col]]);
 
 	return data;
+
+#Selects features based on the kbest selector score
+def select_features(feats, output):
+
+  selector = SelectKBest(chi2, k=5)
+  selector.fit(feats, output);
+  mask = selector.get_support(indices=True)
+  new_features = []
+  feature_names = list(feats.columns.values)
+
+  for bool, feature in zip(mask, feature_names):
+      if bool:
+          new_features.append(feature)
+
+  features_to_remove = list(set(feature_names) - set(new_features));
+  feats = feats.drop(features_to_remove, axis=1);
+
+  return feats;
 
 def main(args):
 	#Load data table
@@ -50,6 +71,8 @@ def main(args):
 	dataAttr = data.drop([OUTPUT_COL], axis=1);
 	dataOut = data[[OUTPUT_COL]];
 
+	dataAttr = select_features(dataAttr, dataOut);
+
 	#Split training and testing sets
 	trainFeats, testFeats, trainOutputs, testOutputs = \
     	train_test_split(dataAttr, dataOut, test_size=0.2);
@@ -57,8 +80,21 @@ def main(args):
    	#Create model using decision tree
 	clf = tree.DecisionTreeClassifier(criterion='entropy');
 	clf = clf.fit(trainFeats, trainOutputs);
+
+	ada = ensemble.AdaBoostClassifier();
+	ada = ada.fit(trainFeats, trainOutputs);
+
+	rf = ensemble.RandomForestClassifier(criterion='entropy');
+	rf = rf.fit(trainFeats, trainOutputs);
+
 	#Perform K-Fold cross validation and get accuracy score
 	m = cross_val_score(clf, dataAttr, dataOut, cv=5, scoring='accuracy')
+	print("K-Foldcross validated score: %f" %np.mean(abs(m)));
+
+	m = cross_val_score(ada, dataAttr, dataOut, cv=5, scoring='accuracy')
+	print("K-Foldcross validated score: %f" %np.mean(abs(m)));
+
+	m = cross_val_score(rf, dataAttr, dataOut, cv=5, scoring='accuracy')
 	print("K-Foldcross validated score: %f" %np.mean(abs(m)));
 
 main(None);
