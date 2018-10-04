573c63b0438a13e1d70d034a2692f369ac5ee508
Craig Barry <s3601725@student.rmit.edu.au>
2018-10-04 19:37:23 +1000

    Setup neural net for training

--- task2.py
+++ task2.py
@@ -3,17 +3,34 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten
 
 def main(args):
-	datagen = ImageDataGenerator(rescale=1./255)
+	#Used to scale pixel values between 0 & 1
+	scaleFactor = 1./255;
+	#Create image generator
+	datagen = ImageDataGenerator(rescale=1.0/255.0)
 
+	batch_size = 128;
+	#Steps per epoch set as recommended by the keras documentation
+	#as number of samples divided by batch size
+	steps_per_epoch = 60000 / batch_size;
+
+	#The number of hidden nodes in the hidden layer
+	hidden_nodes = 1024
+
+	#Number of output classes
+	num_classes = 10;
+	#Number of training epochs
+	epochs = 10;
+
+	#Load training images
 	train_generator = datagen.flow_from_directory(
 		directory=r"./data/mnist/training/",
 		target_size=(28,28),
 		color_mode="grayscale",
 		class_mode="categorical",
-		shuffle=True,
-		seed=42
+		shuffle=True
 	);
 
+	#Load test images
 	test_generator = datagen.flow_from_directory(
 		directory=r"./data/mnist/testing/",
 		target_size=(28,28),
@@ -21,17 +38,18 @@ def main(args):
 		class_mode="categorical"
 	);
 
+	#Creates a sequential neural net model
 	model = Sequential()
-	model.add(Dense(28*28, input_shape=(28, 28, 1), activation='relu'))
+
+	#Add the hidden layer
+	model.add(Dense(hidden_nodes, input_shape=(28, 28, 1), activation='relu'))
 	model.add(Flatten());
-	model.add(Dense(10, activation='sigmoid'))
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	#Output nodes
+	model.add(Dense(num_classes, activation='sigmoid'))
 
-	model.fit_generator(train_generator, steps_per_epoch=60000)
-
-	# for i in x:
-	# 	print(i);
-
-	# print x;
+	#Compile to model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']);
+	#Train the model
+	model.fit_generator(train_generator, steps_per_epoch=steps_per_epoch, epochs=epochs);
 
 main(None);
