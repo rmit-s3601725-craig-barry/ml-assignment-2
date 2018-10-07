3616486a012ff7be034ca9607c557398e58186f7
nolDsouza <s3600251@student.rmit.edu.au>
2018-10-08 06:59:07 +1100

    task2 evaluation

--- task2.py
+++ task2.py
@@ -1,31 +1,68 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
+from sklearn.model_selection import StratifiedKFold
 
+TRAIN_SIZE = 60000
+#Number of samples used per gradient increment
+BATCH_SIZE = 74
+#Keras docs recommend steps per epoch = number of samples / batch size
+STEPS_TRAIN = TRAIN_SIZE * 0.9 / BATCH_SIZE
+STEPS_VALID = TRAIN_SIZE * 0.1 / BATCH_SIZE
+#Number of training epochs
+EPOCHS = 1
+
+#Structure of code taken from Keras issue #1711 - 
+#https://github.com/keras-team/keras/issues/1711#issuecomment-185801662 
+#Comment by KeironO
 def main(args):
+    # K-fold cross validation method
+    from keras.utils import to_categorical
+    n_folds = 5
+    train_generator, test_generator = load_data(False)
+    labels = train_generator.class_indices
+    kfold = StratifiedKFold(n_splits=n_folds, shuffle=True)
+    train_y = to_categorical(train_generator, num_classes=10)
+    test_y = to_categorical(test_generator, num_classes=10)
+
+    # for i, (train, test) in kfold.split(train_y, test_y):
+    print("Running Fold", i+1, "/", n_splits)
+    model = None # Clearing the NN.
+    model = create_model()
+    train_and_evaluate(model, train_generator, test_generator)
+    
+    
+    # Regular train/test method
+    #train_generator, test_generator, valid_generator = load_data()
+    #model = create_model()
+    #train_and_validate(model, train_generator, test_generator, valid_generator)
+
+
+    #Reset seems to be required to align estimated y to actual y
+    #test_generator.reset()
+    #Predict the testing data y values (classify clothing items)
+    #pred = model.predict_generator(
+    #        generator=test_generator,
+    #        verbose=1,
+    #        steps=1000
+    #);
+    #plot(history);
+
+   # save(train_generator, test_generator, pred)
+
+# load data with this function, it can split on validation as option
+def load_data(validation=True):
     #Used to scale pixel values between 0 & 1
-    scaleFactor = 1./255;
+    scaleFactor = 1./255
     #Create image generator
-    datagen = ImageDataGenerator(rescale=1.0/255.0)
-
-    batch_size = 128;
-    #Steps per epoch set as recommended by the keras documentation
-    #as number of samples divided by batch size
-
-    TRAIN_SIZE = 60000
-    STEPS_TRAIN = TRAIN_SIZE * 0.9
-    STEPS_VALID = TRAIN_SIZE * 0.1
-    steps_per_epoch = 60000 / batch_size;
-
-    #The number of hidden nodes in the hidden layer
-    hidden_nodes = 1024
-
-    #Number of output classes
-    num_classes = 10;
-    #Number of training epochs
-    EPOCHS = 1;
+    datagen = ImageDataGenerator(
+            rescale=1.0/255.0,
+            # set aside 10% of training data for validation
+            validation_split=0.1 if validation else 0
+    )
 
     #Load training images
     train_generator = datagen.flow_from_directory(
@@ -33,16 +70,41 @@ def main(args):
             target_size=(28,28),
             color_mode="grayscale",
             class_mode="categorical",
+            subset="training",
             shuffle=True
     );
-
+    
     #Load test images
     test_generator = datagen.flow_from_directory(
             directory=r"./data/mnist/testing/",
             target_size=(28,28),
             color_mode="grayscale",
-            class_mode="categorical"
+            class_mode="categorical",
+            #Total number of testing samples must be divisible by batch size
+            batch_size=10
     );
+
+    if validation:
+        #Load validation images, they are not seen while training
+        valid_generator = datagen.flow_from_directory(
+                directory=r"./data/mnist/training/",
+                target_size=(28,28),
+                color_mode="grayscale",
+                class_mode="categorical",
+                subset="validation",
+                shuffle=True
+        );
+        return train_generator, test_generator, valid_generator
+
+    return train_generator, test_generator
+
+# Creation of the model without data yet
+def create_model():
+    #The number of hidden nodes in the hidden layer, this is current optimiser
+    hidden_nodes = 200
+
+    #Number of output classes
+    num_classes = 10
 
     #Creates a sequential neural net model
     model = Sequential()
@@ -54,12 +116,57 @@ def main(args):
     model.add(Dense(num_classes, activation='sigmoid'))
 
     #Compile to model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']);
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+    
+def train_and_evaluate(model, train_generator, test_generator):
     #Train the model
-    model.fit_generator(
-            generator=train_generator, 
-            steps_per_epoch=steps_per_epoch, 
+    history = model.fit_generator(
+            generator=train_generator,
+            steps_per_epoch=STEPS_TRAIN,
             epochs=EPOCHS
-    );
+    )
+
+def train_and_validate(model, train_generator, test_generator, valid_generator):
+    #Train the model
+    history = model.fit_generator(
+            generator=train_generator,
+            steps_per_epoch=STEPS_TRAIN,
+            validation_data=valid_generator,
+            validation_steps=STEPS_VALID,
+            epochs=EPOCHS
+    )
+
+
+
+#Plot the history for the sake of visualising the models performance
+def plot(history):
+    #print(history.history.keys())
+    #Summarise history for accuracy
+    #Keys come in history object
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    #Summarise history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    
+def save(train_generator, test_generator, pred):
+    predicted_class_indices=np.argmax(pred,axis=1)
+    labels = (train_generator.class_indices)
+    labels = dict((v,k) for k,v in labels.items())
+    predictions = [labels[k] for k in predicted_class_indices]
+    filenames=test_generator.filenames
+    results=pd.DataFrame({"Filename":filenames, "Predictions":predictions})
+    results.to_csv("results.csv",index=False)
 
 main(None);
