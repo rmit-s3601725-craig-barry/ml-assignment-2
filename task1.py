eac8895407429e7805db36efb64eb738730ac168
Craig Barry <s3601725@student.rmit.edu.au>
2018-10-09 19:52:12 +1100

    Final submission

--- task1.py
+++ task1.py
@@ -21,19 +21,78 @@ from sklearn.model_selection import train_test_split, cross_val_score, cross_val
 from sklearn.ensemble import BaggingClassifier, ExtraTreesClassifier, AdaBoostClassifier, RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import PolynomialFeatures, Imputer, LabelEncoder
 
+
+DATA_FILE = 'data/property_prices.csv';
 CLEANED_DATA_FILE = "cleaned_data_file.csv"
 USELESS_COLS = ['id', 'address', 'date'];
 CLASSIFICATION_COLS = ['price_bands', 'council_area', 'region_name', 'suburb', 'realestate_agent', 'type', 'method'];
 OUTPUT_COL = 'price_bands'
+N_OUTPUTS = 7;
 DEBUG = True;
-
-TRAIN_FILE = 'train_data.csv'
-TEST_FILE  = 'test_data.csv'
 
 def pr(msg):
 	if(DEBUG):
 		print(msg);
+
+#Converts all string classes to integer values
+def encodeClassifiers(data, cols):
+	for col in cols:
+		le = LabelEncoder()
+		le.fit(data[[col]]);
+		data[[col]] = le.transform(data[[col]]);
+
+	return data;
+
+#Cleans up NaN values in dataset
+def cleanDataset(data):
+	for i in range(0,N_OUTPUTS):
+		for col in list(data):
+			im = Imputer(strategy='median')
+			im.fit(data.loc[data[OUTPUT_COL] == i, [col]]);
+			data.loc[data[OUTPUT_COL] == i, [col]] = im.transform(data.loc[data[OUTPUT_COL] == i, [col]]);
+
+	return data;
+
+#Selects features based on the kbest selector score
+def select_features(feats, output, n_features):
+	selector = SelectKBest(f_classif, k=n_features)
+	selector.fit(feats, output);
+	mask = selector.get_support(indices=True)
+	new_features = []
+	feature_names = list(feats.columns.values)
+
+	for bool, feature in zip(mask, feature_names):
+		if bool:
+			new_features.append(feature)
+
+	features_to_remove = list(set(feature_names) - set(new_features));
+	feats = feats.drop(features_to_remove, axis=1);
+
+	return feats;
+
+def build_dataset(n_features):
+	print('--Building Dataset--');
+
+	#Get the first dataset
+	data = pd.read_table(DATA_FILE, delimiter=',', index_col = False);
+	data = data.loc[data[OUTPUT_COL] != 'Unknown'];
+	#Remove useless columns
+	data = data.drop(USELESS_COLS, axis=1)
+	#cleanup dataset for processing
+	data = encodeClassifiers(data, CLASSIFICATION_COLS);
+	data = cleanDataset(data);
+
+	#Split data into features/target
+	dataAttr = data.drop([OUTPUT_COL], axis=1);
+	dataOutput = data[[OUTPUT_COL]];
+
+	dataAttr = select_features(dataAttr, dataOutput, n_features);
+
+	data = pd.concat([dataAttr, dataOutput], axis=1, sort=False);
+
+	data.to_csv(CLEANED_DATA_FILE, sep=',', index=False);
 
 def process(clfScores, desc, clf, dataAttr, dataOutput, kFolds):
 	clfs = [
@@ -59,9 +118,8 @@ def process(clfScores, desc, clf, dataAttr, dataOutput, kFolds):
 
 	pr('');
 
-def main(args):
+def train(kfolds):
 	clfScores = [];
-	kfolds = 3;
 	num_top_classifiers = 10;
 
 	#Retrieve data
@@ -101,6 +159,13 @@ def main(args):
 	sortedClassifierList = sorted(clfScores, key=lambda x: -x[1]);
 	#Print the top classifiers
 	for i in range(num_top_classifiers):
-		pr("%d. %s: %.2f%%" %((i+1), sortedClassifierList[i][0], sortedClassifierList[i][1] * 100.0));
+		pr("%d. %s: %.2f%%" %((i+1), sortedClassifierList[i][0], sortedClassifierList[i][1]));
+
+def main(args):
+	n_features = 12;
+	kfolds = 3;
+
+	build_dataset(n_features);
+	train(kfolds);
 
 main(None);
